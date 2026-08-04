fa2961d5fbe0aa6e18fa8ab17b524f0b8e827f44
Adarsh Kumar Debata <adarshdebata00@gmail.com>
2026-08-04 13:37:51 +0530

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -135,7 +135,7 @@ export function Hero() {
                 <div
                   className="h-full w-full bg-cover bg-center"
                   style={{
-                    backgroundImage: `url("https://images.unsplash.com/photo-1726584228201-7d0bab5132c4?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
+                    backgroundImage: `url("https://images.unsplash.com/photo-1785826649815-d48da334071d?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
                   }}
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-ink-950/40 via-transparent to-transparent" />
